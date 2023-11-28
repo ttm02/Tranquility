086db20334a0dd2b18c399a5b52da6b07e867acc
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-11-27 20:16:53 -0700

    fix is_turn_valid for if start already played

--- GameManager.cpp
+++ GameManager.cpp
@@ -147,7 +147,7 @@ bool Turn::is_valid(const PlayArea &area, const std::vector<std::unique_ptr<Card
     assert(hand[card_to_play] != nullptr);
 
     // if at least one start card in hand and not played so far
-    if (area.has_start() && num_start_in_hand(hand) > 0) {
+    if ((not area.has_start()) && num_start_in_hand(hand) > 0) {
         return (hand[card_to_play]->value == Card::START &&
                 cards_to_discard.empty());
     }
