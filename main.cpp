d74ff55fd813cefcd29e77487b660c46bd0af882
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-11-27 19:13:28 -0700

    run multiple games

--- main.cpp
+++ main.cpp
@@ -14,12 +14,8 @@
 #include "HumanPlayer.h"
 #include "BinaryPartitionStrategy.h"
 
-
-
-int main() {
-
-    // TODO use a seed
-    auto rng = std::default_random_engine{};
+template<class R>
+bool simulate_game(R rng, unsigned int num_players) {
 
     std::vector<std::unique_ptr<PlayerAgent>> strategies;
 
@@ -29,10 +25,30 @@ int main() {
 
     bool game_won = GameManager::RunNewGame(std::move(strategies), rng);
 
+    return game_won;
 
-    std::cout << "Game Won? " << game_won << "\n";
+}
 
 
+int main() {
+
+    // TODO use a seed
+    auto rng = std::default_random_engine{};
+    int num_games = 100;
+
+    int num_won = 0;
+    for (int i = 0; i < num_games; ++i) {
+
+        bool game_won = simulate_game(rng, 2);
+
+        std::cout << "Game " << i << " Won? " << game_won << "\n";
+        if (game_won) { num_won++; }
+    }
+
+
+    std::cout << "Won " << num_won << " of " << num_games << "\n";
     return 0;
 
 }
+
+
